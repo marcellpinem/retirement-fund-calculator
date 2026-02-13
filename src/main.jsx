cb7f47dfc454c3d1cf56e6230f79f029b9a76534
Marcellino Pinem <marcellpinem@gmail.com>
2026-02-13 22:38:27 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import RetirementFund from "./components/RetirementFund.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <Navbar />
     <Routes>
       <Route path="/" element={<App />} />
@@ -14,5 +14,5 @@ createRoot(document.getElementById("root")).render(
       <Route path="/pendidikan" element={<App />} />
       <Route path="/darurat" element={<App />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 );
